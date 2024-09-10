3ab3fc2565832a6a330e96f5cc2acb433e68fca8
Philip1715 <philiphalomoan17@gmail.com>
2024-09-10 09:15:00 +0700

    tokopakdika2 jaya x 3

--- tokopakdika/settings.py
+++ tokopakdika/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = 'django-insecure-+u$=52&%if=lhqauv$^*tyg1rt!ed!==t!br(7bqlihn+u$qi!
 DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "philip-halomoan-tokopakdika2.pbp.cs.ui.ac.id"]
-# Nambahin allowed host dan benerin yang tadinya sc ke ac
 
 # Application definition
 
