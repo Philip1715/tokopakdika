bba018d8540a107e68da1260ce7a435c81ca80da
Philip1715 <philiphalomoan17@gmail.com>
2024-11-22 22:42:49 +0700

    11/22

--- main/views.py
+++ main/views.py
@@ -14,7 +14,9 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
-
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.http import JsonResponse
 @login_required(login_url='/login')
 def show_main(request):
     last_login = request.COOKIES.get('last_login', 'Unknown')  # Default ke 'Unknown' jika tidak ada
@@ -134,3 +136,21 @@ def add_product_entry_ajax(request):
     new_product.save()
 
     return HttpResponse(b"CREATED", status=201)
+
+@csrf_exempt
+def create_product_flutter(request):
+    if request.method == 'POST':
+
+        data = json.loads(request.body)
+        new_product = ProductEntry.objects.create(
+            user=request.user,
+            mood=data["product"],
+            mood_intensity=int(data["description"]),
+            feelings=data["price"]
+        )
+
+        new_product.save()
+
+        return JsonResponse({"status": "success"}, status=200)
+    else:
+        return JsonResponse({"status": "error"}, status=401)
